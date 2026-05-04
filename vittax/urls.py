3a732c97320f09d5ce31ce0a921e275e27822be5
raveendrathalluri <raveendrathalluri@gmail.com>
2026-05-04 16:35:39 +0530

    changes

--- vittax/urls.py
+++ vittax/urls.py
@@ -1,24 +1,40 @@
-# vittax/urls.py
+# # vittax/urls.py
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+
+#     path('', include('accounts.urls')),
+#     path('', include('services.urls')),
+#     path('', include('contact.urls')),
+#     path('', include('admindashboard.urls')),
+# ]
+
+# # ✅ Serve MEDIA files
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# # ✅ Serve STATIC files
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# # ✅ Error handlers
+# handler404 = 'django.views.defaults.page_not_found'
+# handler500 = 'django.views.defaults.server_error'
+
+
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # Main homepage
     path('', include('accounts.urls')),
-    path('', include('services.urls')),
-    path('', include('contact.urls')),
-    path('', include('admindashboard.urls')),
+
+    # Other apps
+    path('services/', include('services.urls')),
+    path('contact/', include('contact.urls')),
+    path('dashboard/', include('admindashboard.urls')),
 ]
-
-# ✅ Serve MEDIA files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# ✅ Serve STATIC files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# ✅ Error handlers
-handler404 = 'django.views.defaults.page_not_found'
-handler500 = 'django.views.defaults.server_error'
--- vittax/wsgi.py
+++ vittax/wsgi.py
@@ -3,8 +3,8 @@ path = '/home/dharanikumarnellore/VITTAX'
 if path not in sys.path:
     sys.path.append(path)
 
-from django.core.wsgi import get_wsgi_application
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'vittax.settings'
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
