750dac3d6a09c25aeb87865569536df952f9198f
raveendrathalluri <raveendrathalluri@gmail.com>
2026-05-04 16:13:34 +0530

    change

--- vittax/wsgi.py
+++ vittax/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for vittax project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os
+import sys
+path = '/home/dharanikumarnellore/VITTAX'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vittax.settings')
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vittax.settings'
 
 application = get_wsgi_application()
